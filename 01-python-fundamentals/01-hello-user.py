6c66cb6215ab09458be0ffeac38ead6788705269
igormauriciomota <107084301+igormauriciomota@users.noreply.github.com>
2026-08-18 20:55:34 -0300

    praticando fundamentos

---
+++ 01-python-fundamentals/01-hello-user.py
@@ -0,0 +1,6 @@
+nome = input("Digite seu nome: ").strip().title()
+cidade = input("Digite sua cidade: ").strip().title()
+
+print(f"Ola, {nome}!")
+print(f"Voce mora em {cidade}.")
+
---
+++ 01-python-fundamentals/02-personal-data.py
@@ -0,0 +1,11 @@
+nome = input("Nome: ").strip().title()
+idade = int(input("Idade: "))
+altura = float(input("Altura: "))
+profissao = input("Profissão: ").strip().title()
+
+print("\n---- DADOS CADASTRAIS ----")
+print(f"Nome: {nome}")
+print(f"Idade: {idade}")
+print(f"Altura: {altura}")
+print(f"Profissão: {profissao}")
+      
