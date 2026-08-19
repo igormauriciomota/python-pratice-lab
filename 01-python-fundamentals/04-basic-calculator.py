2f34d8023876cd308e19d7e2e34ff814849a0441
igormauriciomota <107084301+igormauriciomota@users.noreply.github.com>
2026-08-18 21:08:14 -0300

    praticando fundamentos

--- 01-python-fundamentals/04-basic-calculator.py
+++ 01-python-fundamentals/04-basic-calculator.py
@@ -6,3 +6,8 @@ subtracao = numero1 - numero2
 multiplicacao = numero1 * numero2
 divisao = numero1 / numero2
 
+print("\n---- RESULTADOS ----")
+print(f"{numero1} + {numero2} = {soma}")
+print(f"{numero1} - {numero2} = {subtracao:.2f}")
+print(f"{numero1} x {numero2} = {multiplicacao:.2f}")
+print(f"{numero1} / {numero2} = {divisao}")
